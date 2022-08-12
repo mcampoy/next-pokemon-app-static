cbf170b1020ff19b9ab97d5a1c772aa66a180184
Matías Campoy <matiascampoy@gmail.com>
2022-08-12 09:51:48 -0300

    pokemon app done

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -7,23 +7,31 @@ interface Props {
   title?: string
 }
 
-export const Layout: FC <Props> = ({ children, title }) => (
-  <>
-    <Head>
-      <title>{ title || 'Pokémon App' }</title>
-      <meta name="author" content="Matías Campoy" />
-      <meta name="description" content={`Información sobre el pokémon ${title}`} />
-      <meta name="keywords" content={`${title}, pokemon, pokedex`} />
-    </Head>
-    <Navbar />
+export const Layout: FC <Props> = ({ children, title }) => {
 
-    <main style={{
-      padding: '0px 20px'
-    }}>
-      {children}
-    </main>
-  </>
+  const origin = (typeof window === 'undefined') ? '' : window.location.origin;
 
-);
+  return (
+    <>
+      <Head>
+        <title>{ title || 'Pokémon App' }</title>
+        <meta name="author" content="Matías Campoy" />
+        <meta name="description" content={`Información sobre el pokémon ${title}`} />
+        <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+        <meta property="og:title" content={`Información sobre el ${title}`}/>
+        <meta property="og:description" content={`Esta es la página sobre ${title}`} />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
+      </Head>
+      <Navbar />
+
+      <main style={{
+        padding: '0px 20px'
+      }}>
+        {children}
+      </main>
+    </>
+
+)
+};
 
 export default Layout;
